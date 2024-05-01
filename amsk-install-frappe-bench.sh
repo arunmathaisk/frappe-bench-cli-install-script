4441b0fdcd9bf35c86c063255fc19b64490ea38e
Arun Mathai S.K <61804369+arunmathaisk@users.noreply.github.com>
2024-05-01 19:04:39 +0530

    Update amsk-install-frappe-bench.sh

--- amsk-install-frappe-bench.sh
+++ amsk-install-frappe-bench.sh
@@ -26,7 +26,9 @@ sudo service mysql restart
 
 # Install Node.js version 18 using nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 18
 
 # Verify Node.js installation
